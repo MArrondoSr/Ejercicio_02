6165231321b4c50b2f1ffea67cb1f28f66270540
malbec1999 <graficopc@gmail.com>
2026-08-20 21:19:24 -0300

    Modificado

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Ejercicio_02
         {
             //En el campeonato estudiantil llegan a la última etapa los participantes que superen los 80 puntos. 
             //El algoritmo pide el nombre de cada participante y la puntuación de los 3 jurados. Debe mostrar el nombre y el puntaje total de cada participante.        
-            //Nota: cuando se ingrese "FIN" en el nombre del participante finaliza nuestro programa.
+            //Nota: cuando se ingrese "FIN" en el nombre del participante finaliza nuestro programa. Push
 
             string nombre_participante;
             int punt_01;
@@ -16,6 +16,7 @@ namespace Ejercicio_02
             int punt_03;
             int punt_total;
 
+            //push
 
                 Console.Write("Ingrese el nombre del participante: ");
 
